938842279f4a57865dc8205f4858a569c6c2a7e1
rrkas <rrka97wal@gmail.com>
2020-05-29 16:13:30 +0530

    Revert "rebuilt project with java"
    This reverts commit 62589cac

---
+++ native_code_integration/android/app/src/main/java/com/example/native_code_integration/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.native_code_integration;
+
+import android.os.Bundle;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.plugin.common.MethodChannel;
+
+public class MainActivity extends FlutterActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        new MethodChannel();
+    }
+}
